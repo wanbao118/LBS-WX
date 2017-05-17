d1d2df15db3db4a3bb63e9c84cf0cd798d3a6736
AndyYang78 <965998042@qq.com>
2017-05-17 21:43:31 +0800

    Init the respository

---
+++ group-pbox-pvbs-Acct/src/main/java/org/group/pbox/pvbs/Acct/App.java
@@ -0,0 +1,13 @@
+package org.group.pbox.pvbs.Acct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
