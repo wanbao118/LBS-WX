54cf5e6aad2d4e2679140b7ea2e6fc7bf70fe84e
FredMao <112923004@qq.com>
2017-05-25 18:58:28 +0800

    add user html

--- group-pbox-pvbs-util/src/main/java/com/group/pbox/pvbs/util/OperationCode.java
+++ group-pbox-pvbs-util/src/main/java/com/group/pbox/pvbs/util/OperationCode.java
@@ -2,7 +2,7 @@ package com.group.pbox.pvbs.util;
 
 public class OperationCode
 {
-    public static String TRANS_DEPOSIT = "D";
-    public static String TRANS_WITHDRAW = "W";
-    public static String ACCT_CREATION = "A";
+    public static final String TRANS_DEPOSIT = "D";
+    public static final String TRANS_WITHDRAW = "W";
+    public static final String ACCT_CREATION = "A";
 }
