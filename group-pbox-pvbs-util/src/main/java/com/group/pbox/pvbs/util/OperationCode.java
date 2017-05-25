3c2b98dc2a807439f461ccf2ff7839b914bb78f4
AndyYang78 <965998042@qq.com>
2017-05-26 02:19:04 +0800

    Update operationCode

--- group-pbox-pvbs-util/src/main/java/com/group/pbox/pvbs/util/OperationCode.java
+++ group-pbox-pvbs-util/src/main/java/com/group/pbox/pvbs/util/OperationCode.java
@@ -5,6 +5,12 @@ public class OperationCode
     public static final String TRANS_DEPOSIT = "D";
     public static final String TRANS_WITHDRAW = "W";
     public static final String ACCT_CREATION = "A";
+
     public static final String ACCT_MAINTENANCE = "E";
     public static final String ACCT_CLOSURE = "D";
+
+    public static final String ACCT_MAINT = "U";
+    public static final String ACCT_DETL = "D";
+    public static final String FETCH_BY_USER_ID = "FBU";
+
 }
