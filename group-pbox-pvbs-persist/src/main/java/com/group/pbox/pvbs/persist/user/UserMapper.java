e97de55de50ce58a73d4ec580e4dda984aecf4af
AndyYang78 <965998042@qq.com>
2017-05-26 03:01:06 +0800

    update login mapper

--- group-pbox-pvbs-persist/src/main/java/com/group/pbox/pvbs/persist/user/UserMapper.java
+++ group-pbox-pvbs-persist/src/main/java/com/group/pbox/pvbs/persist/user/UserMapper.java
@@ -4,7 +4,7 @@ import com.group.pbox.pvbs.model.user.User;
 
 public interface UserMapper
 {
-    int accountValid(User user);
+    User accountValid(User user);
     boolean addUser(User user);
     int updateUser(User user);
     User fetchUserDetlByUserId(String userId);
