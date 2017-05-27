4401492f7aab8d19b08e8678c6a16f4ecf7438c2
AndyYang78 <965998042@qq.com>
2017-05-27 17:27:09 +0800

    Update the ccyExchange

--- group-pbox-pvbs-clientmodel/src/main/java/com/group/pbox/pvbs/clientmodel/ccyEx/CcyExchangeRateReqModel.java
+++ group-pbox-pvbs-clientmodel/src/main/java/com/group/pbox/pvbs/clientmodel/ccyEx/CcyExchangeRateReqModel.java
@@ -5,12 +5,17 @@ import java.math.BigDecimal;
 import com.group.pbox.pvbs.common.model.BaseClientReqModel;
 
 public class CcyExchangeRateReqModel extends BaseClientReqModel {
-	
+	private String userId;
+
 	private String id;
 	
 	private String currencyCode;
 	
 	private BigDecimal exchangeRate;
+
+	private String acctNumber;
+
+	private double changeAmount;
 
 	public String getId() {
 		return id;
@@ -34,6 +39,30 @@ public class CcyExchangeRateReqModel extends BaseClientReqModel {
 
 	public void setExchangeRate(BigDecimal exchangeRate) {
 		this.exchangeRate = exchangeRate;
+	}
+
+	public String getAcctNumber() {
+		return acctNumber;
+	}
+
+	public void setAcctNumber(String acctNumber) {
+		this.acctNumber = acctNumber;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public double getChangeAmount() {
+		return changeAmount;
+	}
+
+	public void setChangeAmount(double changeAmount) {
+		this.changeAmount = changeAmount;
 	}
 
 }
