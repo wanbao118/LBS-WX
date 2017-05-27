b86776c87c9e31feaa8e96298f8dff265727e79f
AndyYang78 <965998042@qq.com>
2017-05-27 17:32:13 +0800

    add exchangeRate page

---
+++ group-pbox-pvbs-web/src/main/webapp/exchangerate/queryExRate.js
@@ -0,0 +1,49 @@
+function enqueryExRate() {
+	var currExRateReq = {
+		'operationCode': 'FAC'
+	};
+	$.ajax({
+		url : contextPath+"/service/ccyExRate/getCcyExRate",
+		type : "post",
+		contentType : "application/json",
+		dataType : "json",
+		data : JSON.stringify(currExRateReq),
+		success : function(response) {
+			if (response.result == 00000) {			
+				for(var i = 0;i<response.listData.length;i++){
+					$("#selectCurrency").append("<option value='"+response.listData[i].currencyCode+"'>"+response.listData[i].currencyCode+"</option>");
+				}
+			} else {
+				
+			}
+		}
+	});
+}
+
+$(function(){
+
+	enqueryExRate();
+
+});
+
+function getExRate(){
+	var currencyCode = $('#selectCurrency option:selected') .val();
+	var currExRateReq = {
+			'operationCode': 'FBC',
+			'currencyCode': currencyCode
+		};
+	$.ajax({
+		url : contextPath+"/service/ccyExRate/getCcyExRate",
+		type : "post",
+		contentType : "application/json",
+		dataType : "json",
+		data : JSON.stringify(currExRateReq),
+		success : function(response) {
+			if (response.result == 00000) {
+					$("#showRate").text("1 "+ response.listData[0].currencyCode + " = " + response.listData[0].exchangeRate + " RMB");
+			} else {
+				
+			}
+		}
+	});
+}
