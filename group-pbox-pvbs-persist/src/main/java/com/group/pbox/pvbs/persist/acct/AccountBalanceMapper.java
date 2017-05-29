ecf1bc7400bd42b54718fb19207593cfdec85fcc
AndyYang78 <965998042@qq.com>
2017-05-29 13:47:13 +0800

    source format

--- group-pbox-pvbs-persist/src/main/java/com/group/pbox/pvbs/persist/acct/AccountBalanceMapper.java
+++ group-pbox-pvbs-persist/src/main/java/com/group/pbox/pvbs/persist/acct/AccountBalanceMapper.java
@@ -6,16 +6,15 @@ import org.apache.ibatis.annotations.Param;
 
 import com.group.pbox.pvbs.model.acct.AccountBalance;
 
-public interface AccountBalanceMapper
-{
-    AccountBalance getAccountBalance(AccountBalance accountBalance);
+public interface AccountBalanceMapper {
+	AccountBalance getAccountBalance(AccountBalance accountBalance);
 
-    int updateAccountBalance(AccountBalance accountBalance);
+	int updateAccountBalance(AccountBalance accountBalance);
 
-    int insertAccountBalance(AccountBalance accountBalance);
-    
-    double getBalance(@Param("realAccountNum")String realAccountNum);
-    
-	List<AccountBalance> enquireAccountBalance(@Param("accountNum")String accountNum);
-    
+	int insertAccountBalance(AccountBalance accountBalance);
+
+	double getBalance(@Param("realAccountNum") String realAccountNum);
+
+	List<AccountBalance> enquireAccountBalance(@Param("accountNum") String accountNum);
+
 }
