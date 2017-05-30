2431b8940d66399bb61c1ed4a4d564badd828642
AndyYang78 <965998042@qq.com>
2017-05-30 18:27:11 +0800

    update termDeposit Service

--- group-pbox-pvbs-termDeposit/src/main/java/com/group/pbox/pvbs/termdeposit/ITermDepositRateService.java
+++ group-pbox-pvbs-termDeposit/src/main/java/com/group/pbox/pvbs/termdeposit/ITermDepositRateService.java
@@ -1,12 +1,10 @@
 package com.group.pbox.pvbs.termdeposit;
 
-import java.util.List;
-
 import com.group.pbox.pvbs.clientmodel.termdeposit.TermDepositRateRespModel;
 import com.group.pbox.pvbs.clientmodel.termdeposit.TermDepositReqModel;
 
 public interface ITermDepositRateService {
 	public TermDepositRateRespModel inquiryTermDepositRate(TermDepositReqModel termDepositReqModel) throws Exception;
 
-	public List<TermDepositRateRespModel> inquiryAllTermDepositRate() throws Exception;
+	public TermDepositRateRespModel inquiryAllTermDepositRate() throws Exception;
 }
