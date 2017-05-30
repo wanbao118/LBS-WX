6c158475b28ff76299af9f4193616b3043604635
AndyYang78 <965998042@qq.com>
2017-05-30 18:31:35 +0800

    source format

--- group-pbox-pvbs-clientmodel/src/main/java/com/group/pbox/pvbs/clientmodel/termdeposit/TermDepositRateRespModel.java
+++ group-pbox-pvbs-clientmodel/src/main/java/com/group/pbox/pvbs/clientmodel/termdeposit/TermDepositRateRespModel.java
@@ -3,7 +3,9 @@ package com.group.pbox.pvbs.clientmodel.termdeposit;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TermDepositRateRespModel {
+import com.group.pbox.pvbs.common.model.BaseClientRespModel;
+
+public class TermDepositRateRespModel  extends BaseClientRespModel {
 	private List<TermDepositRateRespData> listData = new ArrayList<TermDepositRateRespData>();
 
 	public List<TermDepositRateRespData> getListData() {
