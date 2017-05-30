b858efe2a0eb5919e88bb63574767cedef628c55
Andy Yang <andy.yang@liferay.com>
2017-05-30 15:28:07 +0800

    Delete ITermDepositRateMapper.java

--- group-pbox-pvbs-persist/src/main/java/com/group/pbox/pvbs/persist/termDeposit/ITermDepositRateMapper.java
+++
@@ -1,9 +0,0 @@
-package com.group.pbox.pvbs.persist.termdeposit;
-
-import java.util.List;
-
-import com.group.pbox.pvbs.model.termdeposit.TermDepositRate;
-
-public interface ITermDepositRateMapper {
-	List<TermDepositRate> fetchAllTermDepositRate(); 
-}
